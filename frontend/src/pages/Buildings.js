d7212a5f673e55f31641049fcc3c31fdf4ac921c
jumbledbrian <brianheart15@gmail.com>
2023-04-20 21:00:25 -0400

    conflict

--- frontend/src/pages/Buildings.js
+++ frontend/src/pages/Buildings.js
@@ -3,12 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import '../assets/styles/main.css';
 import './Buildings.css';
 
-<<<<<<< HEAD
 const BuildingContainer = ( {building} ) => {
-=======
-const Building = ( {building} ) => {
-
->>>>>>> 6a713d5 (buildings stylesheet)
     let navigate = useNavigate();
     const handleRouteClick = () => {
         console.log("clicked");
@@ -17,11 +12,7 @@ const Building = ( {building} ) => {
 
     return (
         <div className="buildingContainer" onClick={() => handleRouteClick()}>
-<<<<<<< HEAD
             <div className="buildingImage" style={{backgroundImage: `url(/${building.replace(/\s/g,'')}.png)`}}></div>
-=======
-            <div className="buildingImage" ></div>
->>>>>>> 6a713d5 (buildings stylesheet)
             <div className="buildingLabel">
                 <div className="label">{building}</div>
             </div>
