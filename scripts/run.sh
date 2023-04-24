6c8938b952daddcc27e8d7a9b7cdfab8617911bc
Linus Zwaka <linuszwaka@gmail.com>
2023-04-24 01:43:42 -0400

    scripts updated

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-BASE_DIR=$(dirname $0)
+BASE_DIR="$( cd "$( dirname "$0" )" && pwd )"
+cd $BASE_DIR
 
 echo "starting server..."
 echo
@@ -10,16 +11,16 @@ echo
 for i in {1..10}
 do
 	echo -n "$i..."
-	sleep 1
+	#sleep 1
 done
 
 killall node
 
 # start backend
-cd ../backend/folsom
+cd backend/folsom
 python3 manage.py runserver > ../../logs/backend.log &
 
 # start frontend
-cd ../../frontend
+cd ../frontend/
 npm start > ../logs/frontend.log &
 
