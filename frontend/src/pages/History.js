e2c46624277c9d8d9900967d2a8f34195d876646
Louis Parrinello <chaos@Louiss-MacBook-Pro.local>
2023-07-02 15:44:11 -0400

    table now properly fetches and displays in table, room has display issues

--- frontend/src/pages/History.js
+++ frontend/src/pages/History.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useEffect, useState} from 'react';
 import '../assets/styles/main.css';
 import Box from '@mui/material/Box';
 import {DataGrid, GridColDef} from '@mui/x-data-grid';
@@ -19,22 +19,30 @@ const History = () => {
         else return "----"
     }
 
-
     let [data,setData] = useState([]);
+
+    useEffect(() => {
+        getData();
+    },[]);
 
     let getData = async ()  =>{
         const response = await fetch('http://127.0.0.1:8000/reservations_api/');
         const data = await response.json();
 
+        for(var i=0; i<data.length; i++){
+            data[i]['start_time'] = new Date(data[i]['start_time']);
+            data[i]['end_time'] = new Date(data[i]['end_time']);
+        }
+
         setData(data)
+        console.log(data[0])
     }
 
-    console.log(data)
 
     const columns: GridColDef[] = [
-        { field: 'room_num', headerName: 'Room', width: 100, headerAlign:'center', sortable: true},
-        { field: 'first_name', headerName: 'First name', width: 100, headerAlign:'center', sortable: true, valueGetter: ({value}) => getField(value)},
-        { field: 'last_name', headerName: 'Last name', width: 150, sortable: true, headerAlign:'center', valueGetter: ({value}) => getField(value)},
+        { field: 'room_num', headerName: 'Room', width: 100, sortable: true},
+        { field: 'first_name', headerName: 'First name', width: 100, sortable: true, valueGetter: ({value}) => getField(value)},
+        { field: 'last_name', headerName: 'Last name', width: 150, sortable: true, valueGetter: ({value}) => getField(value)},
         { field: 'start_time', headerName: 'Start Time',width: 100,align: 'center',headerAlign: 'center',sortable: true, valueGetter: ({value}) => getTime(value)},
         { field: 'end_time',headerName: 'End Time',width: 100,align: 'center',headerAlign: 'center',sortable: true, valueGetter: ({value}) => getTime(value)},
     ]
