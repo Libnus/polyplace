c8b5b4c6055cc0a322148b6e78659c599b6aac8b
jumbledbrian <brianheart15@gmail.com>
2023-07-15 22:00:12 -0400

    slight ui changes and updated normal navbar

--- frontend/src/components/Main/Header.js
+++ frontend/src/components/Main/Header.js
@@ -1,23 +1,101 @@
 import React from "react";
-import {NavLink} from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 import "../../assets/styles/main.css";
+import {
+  FaHome,
+  FaTicketAlt,
+  FaBuilding,
+  FaCalendarAlt,
+  FaUsers,
+  FaHandMiddleFinger,
+  FaHistory,
+} from "react-icons/fa";
 
 const Header = () => {
   return (
-    <div className="header">
-      <h1 style={{ left: "20%", color: "white" }}>PolyPlace</h1>
-
-      <nav>
-        <div className="nav-elements">
-          <ul>
-            <li> <a href="/">Home</a></li>
-            <li><a href="/buildings">Reservations</a></li>
-            <li><a href="/rooms">Staff</a></li>
-            <li><a href="/history">History</a></li>
-          </ul>
-        </div>
-      </nav>
-    </div>
+    <>
+      <div className="header">
+        {/*<h1 style={{ left: "20%", color: "white" }}>PolyPlace</h1>*/}
+        <nav>
+          <div className="nav-elements">
+            <ul>
+              <li>
+                <a
+                  style={{
+                    left: "20%",
+                    color: "white",
+                    padding: "18.5px 16px",
+                    backgroundColor: "#c93135",
+                    boxShadow:
+                      "0px 0px 0px 0px rgba(0, 0, 0, 0.5), inset 0px -3px 1px -2px rgba(0, 0, 0, 0.3)",
+                    fontSize: "30px",
+                    fontWeight: "900",
+                  }}
+                >
+                  Polyplace
+                </a>
+              </li>
+              <li>
+                <a href="/">
+                  <div
+                    style={{
+                      verticalAlign: "middle",
+                      display: "inline-flex",
+                      paddingRight: "4px",
+                    }}
+                  >
+                    <FaHome />
+                  </div>
+                  Home
+                </a>
+              </li>
+              <li>
+                <a href="/buildings">
+                  <div
+                    style={{
+                      verticalAlign: "middle",
+                      display: "inline-flex",
+                      paddingRight: "4px",
+                    }}
+                  >
+                    <FaCalendarAlt />
+                  </div>
+                  Reservations
+                </a>
+              </li>
+              <li>
+                <a href="/rooms">
+                  <div
+                    style={{
+                      verticalAlign: "middle",
+                      display: "inline-flex",
+                      paddingRight: "4px",
+                    }}
+                  >
+                    <FaUsers />
+                  </div>
+                  Staff
+                </a>
+              </li>
+              <li>
+                <a href="/history">
+                  <div
+                    style={{
+                      verticalAlign: "middle",
+                      display: "inline-flex",
+                      paddingRight: "4px",
+                    }}
+                  >
+                    <FaHistory />
+                  </div>
+                  History
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </>
   );
 };
 export default Header;
