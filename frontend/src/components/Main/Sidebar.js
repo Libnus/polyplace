510af4f49bb756e370010952506ce20cab32ba7f
jumbledbrian <brianheart15@gmail.com>
2023-07-18 19:42:46 -0400

    start of mobile scaling ui v2

--- frontend/src/components/Main/Sidebar.js
+++ frontend/src/components/Main/Sidebar.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import { slide as Menu } from 'react-burger-menu';
-import './Sidebar.css';
+import React from "react";
+import { slide as Menu } from "react-burger-menu";
+import "./Sidebar.css";
+import { FaHome, FaCalendarAlt, FaUsers, FaHistory } from "react-icons/fa";
 
 const SideBar = () => {
-
   return (
+    <>    
     <Menu right>
+      <div style = {{background: "none", border: "none", borderBottom:"2px solid white", fontWeight: "bold", fontSize: "30px", padding:"0px", position:"relative", top:"5%",paddingBottom:"15%"}}>Polyplace</div>
       <a className="menu-item" href="/">
+        <FaHome style = {{color:"white", paddingRight:"3px"}} />
         Home
       </a>
       <a className="menu-item" href="/rooms">
+        <FaUsers style = {{color:"white",paddingRight:"3px"}}/>
         Rooms
       </a>
       <a className="menu-item" href="/history">
+        <FaHistory style = {{color:"white",paddingRight:"3px"}}/>
         History
       </a>
       <a className="menu-item" href="/buildings">
+        <FaCalendarAlt style = {{color:"white",paddingRight:"3px"}}/>
         Buildings
       </a>
     </Menu>
+    </>
+
   );
 };
 
